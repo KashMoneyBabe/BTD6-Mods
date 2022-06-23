b00f43603c8a7a6b72eeabb98ad3608438ed57a8
KashMoneyBabe <94859453+KashMoneyBabe@users.noreply.github.com>
2022-06-23 19:19:16 +0300

    Add files via upload

--- BetterVillage/Main.cs
+++ BetterVillage/Main.cs
@@ -1,15 +1,11 @@
 ﻿using Assets.Scripts.Models;
 using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Models.Towers.Behaviors;
 using Assets.Scripts.Models.Towers.Mods;
 using BTD_Mod_Helper;
 using BTD_Mod_Helper.Extensions;
 using Il2CppSystem.Collections.Generic;
 using MelonLoader;
-
-
-using Assets.Scripts.Models.Towers.Behaviors;
-using Assets.Scripts.Models.Towers.TowerFilters;
-using UnhollowerBaseLib;
 
 [assembly: MelonInfo(typeof(BetterVillage.Main), "Better Village", "1.0.0", "KashMoneyBabe")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -51,6 +47,7 @@ namespace BetterVillage
                         behavior.filters = null;
                         behavior.maxStackSize = 999;
                         behavior.isUnique = false;
+                        behavior.pierce += 2f;
                     }
                 }
                 if (towerModel.HasBehavior<ProjectileSpeedSupportModel>())
@@ -60,6 +57,8 @@ namespace BetterVillage
                         behavior.filters = null;
                         behavior.maxStackSize = 999;
                         behavior.isUnique = false;
+                        behavior.multiplier = 1f;
+
                     }
                 }
                 if (towerModel.HasBehavior<AbilityCooldownScaleSupportModel>())
@@ -69,6 +68,7 @@ namespace BetterVillage
                         behavior.filters = null;
                         behavior.maxStackSize = 999;
                         behavior.isUnique = false;
+                        behavior.abilityCooldownSpeedScale += 0.15f;
                     }
                 }
                 if (towerModel.HasBehavior<FreeUpgradeSupportModel>())
@@ -76,6 +76,7 @@ namespace BetterVillage
                     foreach (var behavior in towerModel.GetBehaviors<FreeUpgradeSupportModel>())
                     {
                         behavior.filters = null;
+                        behavior.upgrade += 1;
                     }
                 }
 
